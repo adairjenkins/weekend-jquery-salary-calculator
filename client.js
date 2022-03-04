ef6db08fcd757d8694e5e2747353ee5baf5e4f50
Maggie Jenkins <adair.jenkins@gmail.com>
2022-03-04 15:58:48 -0600

    add addEmployee and updateDOM functions

--- client.js
+++ client.js
@@ -1,9 +1,49 @@
-console.log('js loaded');
+console.log('js');
 
 $(document).ready(handleReady);
 
- function handleReady() {
-     console.log('jquery loaded');
+let employees = []; 
 
-     
+function handleReady() {
+     console.log('jquery');
+    // 
+     $('#submitButton').on('click', addEmployee);
+ }
+
+ function addEmployee() {
+    console.log('addEmployee');
+    // create new employee object from inputs
+    const employee = {
+        firstName: $('#firstName').val(),
+        lastName: $('#lastName').val(),
+        idNum: Number($('#idNum').val()),
+        title: $('#title').val(),
+        salary: $('#salary').val()
+    }
+    // add employee to employee array
+    employees.push(employee);
+    // empty input fields
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#idNum').val('');
+    $('#title').val('');
+    $('#salary').val('');
+
+    updateDOM()
+ }
+
+ function updateDOM() {
+    console.log('updateDOM')
+
+    $('#tableBody').empty()
+
+    for (let employee of employees) {
+        $('#tableBody').append(`<tr>
+            <td>${employee.firstName}</td>
+            <td>${employee.lastName}</td>
+            <td>${employee.idNum}</td>
+            <td>${employee.title}</td>
+            <td>${employee.salary}</td>
+        </tr>`)
+    }
  }
